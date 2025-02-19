46f3e6c74dfb354f5218124055922e91502e4ae0
Martijn Govers <Martijn.Govers@Alliander.com>
2025-02-19 10:22:20 +0100

    set CPU_COUNT
    Signed-off-by: Martijn Govers <Martijn.Govers@Alliander.com>

--- .scripts/run_win_build.bat
+++ .scripts/run_win_build.bat
@@ -73,7 +73,9 @@ if NOT [%flow_run_id%] == [] (
 call :end_group
 
 :: Build the recipe
-echo Building recipe
+echo Building recipe:
+echo conda-build.exe "recipe" -m .ci_support\%CONFIG%.yaml --suppress-variables %EXTRA_CB_OPTIONS%
+echo Start
 conda-build.exe "recipe" -m .ci_support\%CONFIG%.yaml --suppress-variables %EXTRA_CB_OPTIONS%
 if !errorlevel! neq 0 exit /b !errorlevel!
 
