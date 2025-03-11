cf1d1e1cb6fa5e74ba4ed74c4e6ebecb7df2f179
Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>
2025-03-11 13:34:17 +0100

    add scripts
    Signed-off-by: Tony Xiang <19280867+TonyXiang8787@users.noreply.github.com>

---
+++ recipe/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo ${PKG_VERSION} > VERSION
+
+cmake ${CMAKE_ARGS} \
+      -GNinja \
+      -DCMAKE_BUILD_TYPE=Release \
+      -B build/ \
+      -S . \
+      -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+cmake --build build/ --verbose -j1
+
+cmake --install build/
+
+POWER_GRID_MODEL_NO_BINARY_BUILD=1 ${PYTHON} -m pip install . -vv --no-build-isolation --no-deps   
