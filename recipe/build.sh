e7765ff18e930bb3c4ac59e1e967d7b4a6133932
Santiago Figueroa Manrique <figueroa1395@gmail.com>
2025-07-14 09:34:28 +0200

    fix
    Signed-off-by: Santiago Figueroa Manrique <figueroa1395@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+# Get c++20 to work
+if [[ $(uname) == "Darwin" ]]; then
+  export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 echo ${PKG_VERSION} > VERSION
 
 cmake ${CMAKE_ARGS} \
